a3d3c162ddd1a4adf3de99cef36d733704c57b3d
Nupur Sikka <67430495+Nupur2001@users.noreply.github.com>
2025-02-18 18:02:28 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,18 +1,24 @@
-document.addEventListener("DOMContentLoaded",()=>{
-    let container=document.querySelector(".container")
-    let textarea=document.querySelector("textarea")
-    let keywords=document.querySelector(".keywords")
+document.addEventListener("DOMContentLoaded", () => {
+  let container = document.querySelector(".container");
+  let textarea = document.querySelector("textarea");
+  let keywords = document.querySelector(".keywords");
 
-    textarea.addEventListener("keydown",(e)=>{
-        keywordCreate(e.target.value)
-        console.log(e.key)
-    })
+  textarea.addEventListener("keydown", (e) => {
+    keywordCreate(e.target.value);
+  });
 
-    function keywordCreate(){
-        let key=document.createElement("span")
-        key.classList.add("keyword")
-        keywords.append(key)
-        
-    }
-    
-})
+  function keywordCreate(i) {
+    let choices = i
+      .split(",")
+      .map((choice) => choice.trim())
+      .filter((choice) => choice.trim() !== "");
+    console.log(choices);
+
+    choices.forEach((choice) => {
+      let key = document.createElement("span");
+      key.classList.add("keyword");
+      key.innerHTML = choice;
+      keywords.appendChild(key);
+    });
+  }
+});
