6315bd0d96f0a92befa2f8983b06a2ccf325f168
Nupur Sikka <67430495+Nupur2001@users.noreply.github.com>
2025-02-19 10:57:19 +0530

    Update script.js

--- script.js
+++ script.js
@@ -3,8 +3,18 @@ document.addEventListener("DOMContentLoaded", () => {
   let textarea = document.querySelector("textarea");
   let keywords = document.querySelector(".keywords");
 
-  textarea.addEventListener("keydown", (e) => {
+  textarea.addEventListener("keyup", (e) => {
+    let enterClickCount=0
+    keywords.innerHTML = "";
     keywordCreate(e.target.value);
+
+    if (e.key === "Enter") {
+      randomKeywordSelect();
+      textarea.value=""
+    }
+    if (enterClickCount===2) {
+      keywordsRemove()
+    }
   });
 
   function keywordCreate(i) {
@@ -21,4 +31,26 @@ document.addEventListener("DOMContentLoaded", () => {
       keywords.appendChild(key);
     });
   }
+
+  function randomKeywordSelect() {
+    let allKeywords = document.querySelectorAll(".keyword");
+    if (allKeywords === "") {
+      return;
+    }
+    let randomKeyword =
+      allKeywords[Math.floor(Math.random() * allKeywords.length)];
+    let selectedKeyword = document.createElement("span");
+    selectedKeyword.classList.add("highlight");
+    function highlight() {
+      randomKeyword.classList.add("highlight");
+    }
+    setTimeout(highlight(), 500);
+    console.log(randomKeyword);
+  }
+
+  function keywordsRemove(){
+    let allKeywords = document.querySelectorAll(".keyword");
+    allKeywords.remove()
+  }
 });
+//   allKeywords,nokeywords,randomKeywords
